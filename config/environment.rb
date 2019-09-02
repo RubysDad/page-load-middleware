b4cd68f10a6aa17278de9e2624a936d19f654b07
Rubysdad <marktravismorales@gmail.com>
2019-09-02 16:19:04 -0500

    Created a response timer middleware for page loads

---
+++ config/environment.rb
@@ -0,0 +1,9 @@
+# Load the Rails application.
+require_relative 'application'
+
+# Initialize the Rails application.
+Rails.application.initialize!
+
+# Rails::Initializer.run do |config|
+#   config.middleware.use "ResponseTimer"
+# end
